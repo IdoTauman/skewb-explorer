234a3ce56867b5a5cb40261e73588aec7befb964
Ido Tauman <ido032009@gmail.com>
2024-02-05 15:23:07 +0200

    Added state editor (still needs frontend stuff)

---
+++ app/main.py
@@ -0,0 +1,7 @@
+from cube_state import CubeState
+
+cube = CubeState([0, 1, 2, 3],
+            [0, 1, 2, 3],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 1, 2, 3, 4, 5])
---
+++ app/state_editor.py
@@ -0,0 +1,44 @@
+from main import cube
+
+
+color = None
+
+
+def change_color_white():
+    global color
+    color = "white"
+
+
+def change_color_green():
+    global color
+    color = "green"
+
+
+def change_color_red():
+    global color
+    color = "red"
+
+
+def change_color_blue():
+    global color
+    color = "blue"
+
+
+def change_color_orange():
+    global color
+    color = "orange"
+
+
+def change_color_yellow():
+    global color
+    color = "yellow"
+
+
+def get_clicked_sticker():
+    #Do this in frontend
+    pass
+
+
+def change_sticker_color(sticker, color):
+    #sticker.color = color
+    pass
