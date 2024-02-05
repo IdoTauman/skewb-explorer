ea71d71cb1c01b77f18dd83931864c66c1cfc46c
Ido Tauman <ido032009@gmail.com>
2024-02-05 14:43:39 +0200

    Added R and R'

--- app/cube_state.py
+++ app/cube_state.py
@@ -1,9 +1,15 @@
 class CubeState:
     def __init__(self, state) -> None:
         self.state = state
-    
+
     def solved(self):
-        self.state = [[0, 1, 2, 3], [0, 1, 2, 3], [0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 2, 3, 4, 5]]
+        self.state = [
+            [0, 1, 2, 3],
+            [0, 1, 2, 3],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 1, 2, 3, 4, 5],
+        ]
 
     def is_solvable(self):
         # Parity check
@@ -18,7 +24,7 @@ class CubeState:
                 temp_state[0][v] = v
                 temp_state[0][i] = temp_arg
                 corner_swaps += 1
-        
+
         for i, v in enumerate(temp_state[1]):
             if not i == v:
                 temp_arg = temp_state[1][v]
@@ -32,24 +38,95 @@ class CubeState:
                 temp_state[4][v] = v
                 temp_state[4][i] = temp_arg
                 center_swaps += 1
-        
-        if corner_swaps%2 == 1 or alt_corner_swaps%2 == 1 or center_swaps%2 == 1:
+
+        if corner_swaps % 2 == 1 or alt_corner_swaps % 2 == 1 or center_swaps % 2 == 1:
             return False
-        
-        #Corner orientation check
+
+        # Corner orientation check
         sum = 0
         for i in temp_state[2]:
             sum += i
-        if sum%3 != 0:
+        if sum % 3 != 0:
             return False
-        
+
         sum = 0
         for i in temp_state[3]:
             sum += i
-        if sum%3 != 0:
+        if sum % 3 != 0:
             return False
-        
+
         return True
 
-cube = CubeState([[0, 1, 2, 3], [0, 1, 2, 3], [0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 2, 3, 4, 5]])
-print(cube.is_solvable())
+    def move_R(self, reps):
+        temp_state = self.state.copy()
+        if reps == 1:
+            self.state = [
+                [
+                    temp_state[0][0],
+                    temp_state[0][2],
+                    temp_state[0][3],
+                    temp_state[0][1],
+                ],
+                [
+                    temp_state[1][0],
+                    temp_state[1][1],
+                    temp_state[1][2],
+                    temp_state[1][3],
+                ],
+                [
+                    (temp_state[2][0]) % 3,
+                    (temp_state[2][1] + 2) % 3,
+                    (temp_state[2][2] + 2) % 3,
+                    (temp_state[2][3] + 2) % 3,
+                ],
+                [
+                    (temp_state[3][0]) % 3,
+                    (temp_state[3][1]) % 3,
+                    (temp_state[3][2]) % 3,
+                    (temp_state[3][3] + 1) % 3,
+                ],
+                [
+                    temp_state[4][0],
+                    temp_state[4][1],
+                    temp_state[4][5],
+                    temp_state[4][2],
+                    temp_state[4][4],
+                    temp_state[4][3],
+                ]
+            ]
+
+        if reps == 2:
+            self.state = [
+                [
+                    temp_state[0][0],
+                    temp_state[0][3],
+                    temp_state[0][1],
+                    temp_state[0][2],
+                ],
+                [
+                    temp_state[1][0],
+                    temp_state[1][1],
+                    temp_state[1][2],
+                    temp_state[1][3],
+                ],
+                [
+                    (temp_state[2][0]) % 3,
+                    (temp_state[2][1] + 1) % 3,
+                    (temp_state[2][2] + 1) % 3,
+                    (temp_state[2][3] + 1) % 3,
+                ],
+                [
+                    (temp_state[3][0]) % 3,
+                    (temp_state[3][1]) % 3,
+                    (temp_state[3][2]) % 3,
+                    (temp_state[3][3] + 2) % 3,
+                ],
+                [
+                    temp_state[4][0],
+                    temp_state[4][1],
+                    temp_state[4][3],
+                    temp_state[4][5],
+                    temp_state[4][4],
+                    temp_state[4][2],
+                ]
+            ]
