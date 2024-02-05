2b4c56b465995a954c79855a37b34ec310afa51e
Ido Tauman <ido032009@gmail.com>
2024-02-05 14:13:17 +0200

    Added is_solvable

--- app/cube_state.py
+++ app/cube_state.py
@@ -3,23 +3,53 @@ class CubeState:
         self.state = state
     
     def solved(self):
-        self.state = [[0, 1, 2, 3, 4, 5, 6, 7], [0, 1, 2, 3, 4, 5]]
+        self.state = [[0, 1, 2, 3], [0, 1, 2, 3], [0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 2, 3, 4, 5]]
 
-    def is_solvable(self, state):
-        temp_state = state.copy()
-        temp_state2 = state.copy()
-        temp_state.sort()
+    def is_solvable(self):
+        # Parity check
+        temp_state = self.state.copy()
         temp_arg = 0
         corner_swaps = 0
-        for i, v in enumerate(temp_state2[0]):
+        alt_corner_swaps = 0
+        center_swaps = 0
+        for i, v in enumerate(temp_state[0]):
             if not i == v:
-                temp_arg = temp_state2[0][v]
-                temp_state2[0][v] = v
-                temp_state2[0][i] = temp_arg
+                temp_arg = temp_state[0][v]
+                temp_state[0][v] = v
+                temp_state[0][i] = temp_arg
                 corner_swaps += 1
-            if temp_state2 == temp_state:
-                break
-        return corner_swaps
+        
+        for i, v in enumerate(temp_state[1]):
+            if not i == v:
+                temp_arg = temp_state[1][v]
+                temp_state[1][v] = v
+                temp_state[1][i] = temp_arg
+                alt_corner_swaps += 1
 
-cube = CubeState([[1, 0, 3, 2, 5, 4, 6, 7], [0, 1, 2, 3, 4, 5]])
-print(cube.is_solvable(cube.state))
+        for i, v in enumerate(temp_state[4]):
+            if not i == v:
+                temp_arg = temp_state[4][v]
+                temp_state[4][v] = v
+                temp_state[4][i] = temp_arg
+                center_swaps += 1
+        
+        if corner_swaps%2 == 1 or alt_corner_swaps%2 == 1 or center_swaps%2 == 1:
+            return False
+        
+        #Corner orientation check
+        sum = 0
+        for i in temp_state[2]:
+            sum += i
+        if sum%3 != 0:
+            return False
+        
+        sum = 0
+        for i in temp_state[3]:
+            sum += i
+        if sum%3 != 0:
+            return False
+        
+        return True
+
+cube = CubeState([[0, 1, 2, 3], [0, 1, 2, 3], [0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 2, 3, 4, 5]])
+print(cube.is_solvable())
